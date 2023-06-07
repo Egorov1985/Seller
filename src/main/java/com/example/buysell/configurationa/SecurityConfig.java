56bae1e352b415d89543b9da994c931f8d7e9379
Egorov1985 <122155336+Egorov1985@users.noreply.github.com>
2023-06-07 17:12:34 +0300

    "update 07"

--- src/main/java/com/example/buysell/configurationa/SecurityConfig.java
+++ src/main/java/com/example/buysell/configurationa/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
                 .antMatchers("/", "/product/**",
-                        "/image/**", "/registration", "/images/**")
+                        "/image/**", "/registration", "/images/**", "/user/**")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
--- src/main/java/com/example/buysell/services/UserService.java
+++ src/main/java/com/example/buysell/services/UserService.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -23,11 +25,23 @@ public class UserService  {
         }
         user.setActive(true);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.getRoles().add(Role.ROLE_USER);
+        user.getRoles().add(Role.ROLE_ADMIN);
         userRepository.save(user);
         log.info("Saving new User with email: {}", email);
         return true;
     }
 
+    public List<User> listUser(){
+        return userRepository.findAll();
+    }
 
+
+    public void banUser(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user!=null){
+            user.setActive(false);
+        }
+        log.info("Ban user with id = {}; email = {}", user.getId(), user.getEmail());
+        userRepository.save(user);
+    }
 }
